432d3aae6a4f06936df9562f85d09b40b82e83e8
Bita <b.tarfiee@gmail.com>
2025-07-06 00:08:43 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+# filename: app.py
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+CHROMA_PATH = "chroma"
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+app = FastAPI()
+
+embedding_function = OpenAIEmbeddings()
+db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+model = ChatOpenAI()
+
+class Query(BaseModel):
+    query_text: str
+
+@app.post("/chat")
+async def chat(query: Query):
+    query_text = query.query_text
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(results) == 0 or results[0][1] < 0.7:
+        return {"response": "Unable to find matching results.", "sources": []}
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _ in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+
+    response_text = model.invoke(prompt)
+    sources = [doc.metadata.get("source", None) for doc, _ in results]
+    return {"response": response_text, "sources": sources}
